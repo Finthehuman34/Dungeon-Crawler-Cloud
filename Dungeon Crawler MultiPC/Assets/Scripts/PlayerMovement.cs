3f929ab45879738e40d5e3a578b7338a80389e71
Finthehuman34 <finthehuman34@gmail.com>
2024-01-23 09:54:52 +0000

    animation

--- Dungeon Crawler MultiPC/Assets/Scripts/PlayerMovement.cs
+++ Dungeon Crawler MultiPC/Assets/Scripts/PlayerMovement.cs
@@ -7,12 +7,17 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 5f; // set movement speed of the player
 
     public Rigidbody2D rb; // creates a variable to access the rigidbody of the player
-
+    public Animator animator;
     Vector2 movement; // store the x and y movements of the player
     void Update() // used for player input
     {
         movement.x = Input.GetAxisRaw("Horizontal"); // get the input for horizontal movement
         movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal",movement.x);
+        animator.SetFloat("Vertical",movement.y);
+        animator.SetFloat("Speed",movement.sqrMagnitude);
+        
     }
 
     void FixedUpdate() // used for updating movement faster
