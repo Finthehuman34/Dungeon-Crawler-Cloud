59101c347ead2363370f4b6f0b6467f2277283d7
Finthehuman34 <finthehuman34@gmail.com>
2024-01-31 22:11:58 +0000

    WORKS

--- Dungeon Crawler MultiPC/Assets/Scripts/Pathfinding.cs
+++ Dungeon Crawler MultiPC/Assets/Scripts/Pathfinding.cs
@@ -1,9 +1,8 @@
-
+﻿
 
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using CodeMonkey.Utils;
 
 public class Pathfinding {
 
--- Dungeon Crawler MultiPC/Assets/Scripts/Testing.cs
+++ Dungeon Crawler MultiPC/Assets/Scripts/Testing.cs
@@ -4,37 +4,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using CodeMonkey.Utils;
-
+using CodeMonkey;
 
 public class Testing : MonoBehaviour {
     
+    [SerializeField] private PathfindingDebugStepVisual pathfindingDebugStepVisual;
+    [SerializeField] private PathfindingVisual pathfindingVisual;
     
-
-
     private Pathfinding pathfinding;
 
     private void Start() {
         pathfinding = new Pathfinding(227, 162);
-        
+        pathfindingDebugStepVisual.Setup(pathfinding.GetGrid());
+        pathfindingVisual.SetGrid(pathfinding.GetGrid());
     }
 
     private void Update() {
         if (Input.GetMouseButtonDown(0)) {
             Vector3 mouseWorldPosition = UtilsClass.GetMouseWorldPosition();
             pathfinding.GetGrid().GetXY(mouseWorldPosition, out int x, out int y);
-            List<PathNode> path = pathfinding.FindPath(3, -2, x, y);
-            Debug.Log(mouseWorldPosition);
-            Debug.Log(path);
+            List<PathNode> path = pathfinding.FindPath(0, 0, x, y);
             if (path != null) {
                 for (int i=0; i<path.Count - 1; i++) {
-                    Debug.DrawLine(new Vector3(path[i].x, path[i].y) * 5f + Vector3.one * 2.5f, new Vector3(path[i+1].x, path[i+1].y) * 5f + Vector3.one * 2.5f, Color.green, 2.5f);
-                    Debug.Log("Path");
+                    Debug.DrawLine(new Vector3(path[i].x, path[i].y) * 1f + Vector3.one * 0.5f, new Vector3(path[i+1].x, path[i+1].y) * 1f + Vector3.one * 0.5f, Color.green, 0.05f);
                 }
             }
             
         }
 
-        
+        if (Input.GetMouseButtonDown(1)) {
+            Vector3 mouseWorldPosition = UtilsClass.GetMouseWorldPosition();
+            pathfinding.GetGrid().GetXY(mouseWorldPosition, out int x, out int y);
+            pathfinding.GetNode(x, y).SetIsWalkable(!pathfinding.GetNode(x, y).isWalkable);
+        }
     }
 
 }
