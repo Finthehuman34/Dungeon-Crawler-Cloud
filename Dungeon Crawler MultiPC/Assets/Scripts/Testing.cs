eea9ad36ea6fec91fbc57b33a41bbd6cd79a1979
Finthehuman34 <finthehuman34@gmail.com>
2024-01-31 12:45:54 +0000

    just more debugging

--- Dungeon Crawler MultiPC/Assets/Scripts/Testing.cs
+++ Dungeon Crawler MultiPC/Assets/Scripts/Testing.cs
@@ -24,6 +24,7 @@ public class Testing : MonoBehaviour {
             pathfinding.GetGrid().GetXY(mouseWorldPosition, out int x, out int y);
             List<PathNode> path = pathfinding.FindPath(3, -2, x, y);
             Debug.Log(mouseWorldPosition);
+            Debug.Log(path);
             if (path != null) {
                 for (int i=0; i<path.Count - 1; i++) {
                     Debug.DrawLine(new Vector3(path[i].x, path[i].y) * 5f + Vector3.one * 2.5f, new Vector3(path[i+1].x, path[i+1].y) * 5f + Vector3.one * 2.5f, Color.green, 2.5f);
